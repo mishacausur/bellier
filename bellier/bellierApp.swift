9d36492d6f1965b56e0356b5e64c7bbaa865f6bf
Misha Causur <mishacausur@mail.ru>
2022-07-22 23:12:11 +0500

    Initial Commit

---
+++ bellier/bellierApp.swift
@@ -0,0 +1,17 @@
+//
+//  bellierApp.swift
+//  bellier
+//
+//  Created by Misha Causur on 22.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct bellierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
