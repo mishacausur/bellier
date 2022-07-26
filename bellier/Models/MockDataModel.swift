7388373993f8355d5eef34f986072c37d218f938
Misha Causur <mishacausur@mail.ru>
2022-07-26 23:27:00 +0500

    MockDataModel initial added

---
+++ bellier/Models/MockDataModel.swift
@@ -0,0 +1,12 @@
+//
+//  MockDataModel.swift
+//  bellier
+//
+//  Created by Misha Causur on 26.07.2022.
+//
+
+import Foundation
+
+struct MockDataModel: Codable {
+    var id = UUID().uuidString
+}
